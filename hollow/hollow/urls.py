90d9160de81d9449294a49c5bc7f1ef23665279e
Oryntayev-Mukhammed <hinatarocklee@gmail.com>
2023-02-12 19:55:01 +0600

    12.02.2023

--- hollow/hollow/urls.py
+++ hollow/hollow/urls.py
@@ -13,14 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from school.views import *
+
+from hollow import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('school.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler500 = serverError
 handler404 = pageNotFound
