083790806f252d36fe97e405a3914c5f8fb6f3f0
Oryntayev-Mukhammed <mukhammed.oryntayev@narxoz.kz>
2023-03-16 20:54:48 +0600

    12.02.2023

--- hollow/hollow/settings.py
+++ hollow/hollow/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'hollow.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
